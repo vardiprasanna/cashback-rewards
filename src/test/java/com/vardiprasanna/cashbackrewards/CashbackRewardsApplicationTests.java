9b95d6a3d2de1104bc221f6751274d3929e83017
Prasannakumar B Vardi <vardiprasanna@Prasannakumars-MacBook-Air.local>
2026-07-26 18:15:57 -0700

    initial project setup

---
+++ src/test/java/com/vardiprasanna/cashbackrewards/CashbackRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vardiprasanna.cashbackrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbackRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
